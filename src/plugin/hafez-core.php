1f456c237ccfc04edea4d99bff1e8f6c537f0f80
Alexander.Bet <dev@alexander.bet>
2023-01-14 21:15:08 +0300

    wordpress starting theme and Docker file

---
+++ src/plugin/hafez-core.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Hafez Theme Core
+ * Plugin URI: https://alexander.bet
+ * Description: This plugin will load necessary core for Hafez Theme like: Custom Post Types, Custom Taxonomies, Custom Metaboxes, Custom Widgets etc.
+ * Version: 1.0
+ * Author: Alexander Bet
+ * Author URI: https://github.com/alexander-bet
+ * License: GPL v2
+ */
+
+define('HAFEZ_PLUGIN_URL', plugin_dir_url(__FILE__));
